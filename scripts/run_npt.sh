f29ad3a8f1096968a6e63732efb5e1d4fdd75996
8104 <johannes.karwounopoulos@tuwien.ac.at>
2025-10-10 14:16:40 +0000

    updating sh script

--- scripts/run_npt.sh
+++ scripts/run_npt.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-#SBATCH -p gpu
 
+#SBATCH --partition=GPU-a40s       # select a partition i.e. "GPU-a100"
+#SBATCH --gres=gpu:1                # Use GPU
+#SBATCH --nodes=1                   # select number of nodes
+#SBATCH --ntasks-per-node=12        # select number of tasks per node
+#SBATCH --time=7-00:00:00           # Maximum time
 
 # Load any required modules
 source ~/miniconda3/etc/profile.d/conda.sh
