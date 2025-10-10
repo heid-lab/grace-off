7d89d0bfbe04467ce301499c6cbad1c1b793c6f5
8104 <johannes.karwounopoulos@tuwien.ac.at>
2025-10-10 14:16:40 +0000

    updating sh script

--- scripts/run_gas.sh
+++ scripts/run_gas.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-#SBATCH -p gpu
 
+#SBATCH --partition=GPU-a40s       # select a partition i.e. "GPU-a100"
+#SBATCH --gres=gpu:1                # Use GPU
+#SBATCH --nodes=1                   # select number of nodes
+#SBATCH --ntasks-per-node=12        # select number of tasks per node
+#SBATCH --time=7-00:00:00           # Maximum time
 
 # Load any required modules
 source ~/miniconda3/etc/profile.d/conda.sh
@@ -9,3 +13,6 @@ conda activate ai-fennel
 sol="wat"
 
 python gas.py --model_size small --model_type mace --sol $sol
+
+# for using a GRACE model trained on a_wpS dataset
+#python gas.py --model_size small --model_type grace --sol $sol --dataset a_wpS
