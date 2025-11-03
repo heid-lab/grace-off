1167c79c38ca1bc35c340f93f918d80d5c3bf064
8104 <johannes.karwounopoulos@tuwien.ac.at>
2025-11-03 12:37:48 +0000

    adding the 2 layer models

---
+++ models/2l/b_off_medium/train_grace.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --partition=GPU-a100  # select a partition
+#SBATCH --nodes=1  # select number of nodes (max 2, typically 1)
+#SBATCH --ntasks-per-node=12  # select number of tasks per node (essentially CPU cores)
+#SBATCH --time=7-00:00:00  # request 7 days of max runtime
+#SBATCH --gres=gpu:a100:1
+
+echo "Node(s): $SLURM_JOB_NODELIST"
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+nvidia-smi --query-gpu=index,name,uuid --format=csv,noheader
+
+
+source /home/johannes.karwounopoulos/micromamba/etc/profile.d/mamba.sh
+micromamba activate /home/johannes.karwounopoulos/micromamba/envs/grace-linus
+
+# export TF_USE_LEGACY_KERAS=1
+
+gracemaker -r
+
+gracemaker -r -s
