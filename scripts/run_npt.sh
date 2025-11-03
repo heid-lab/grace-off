5429da6be21a77539e3ec702a7f3336f45e0e832
8104 <johannes.karwounopoulos@tuwien.ac.at>
2025-11-03 12:11:32 +0000

    add new npt,nvt and gas phase scripts as well as corresponding submit scripts

--- scripts/run_npt.sh
+++ scripts/run_npt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --partition=GPU-a40       # select a partition i.e. "GPU-a100"
+#SBATCH --partition=GPU-l40s      # select a partition i.e. "GPU-a100"
 #SBATCH --gres=gpu:1                # Use GPU
 #SBATCH --nodes=1                   # select number of nodes
 #SBATCH --ntasks-per-node=12        # select number of tasks per node
@@ -10,9 +10,23 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate ai-fennel
 
+echo "Node(s): $SLURM_JOB_NODELIST"
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+nvidia-smi --query-gpu=index,name,uuid --format=csv,noheader
+
+model_size=$1
+model_type=$2
+default_dtype=$3
+layer=$4
+dataset=$5
+
 sol="wat"
 
-python ase_npt.py --model_size small --model_type mace --sol $sol
+echo "Running NPT with model_size=$model_size, model_type=$model_type, default_dtype=$default_dtype, layer=$layer, dataset=$dataset"
+python ase_npt.py --model_size $model_size --model_type $model_type --sol $sol --default_dtype $default_dtype --layer $layer --dataset $dataset
+
+
+# python ase_npt.py --model_size small --model_type mace --sol $sol --default_dtype float32
 
 # for using a GRACE model trained on a_wpS dataset
-#python ase_npt.py --model_size small --model_type grace --sol $sol --dataset a_wpS
+# python ase_npt.py --model_size small --model_type grace --sol $sol --dataset a_wpS --layer 1 --default_dtype float32
