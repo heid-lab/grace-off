5eab0be11feefe1c8624bdebb543bfe241a638d1
AnnaPicha <anna.picha@gmail.com>
2026-01-27 16:36:49 +0100

    add latest updates to jupyter notebooks

--- analysis/torsion_scans/submit.sh
+++ analysis/torsion_scans/submit.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#SBATCH -p 4090
+#SBATCH -p ADA
 
 
 # Load any required modules
 source ~/miniconda3/etc/profile.d/conda.sh
-conda activate ai-fennel_2
+conda activate grace-off
+# conda activate ai-fennel_2
 
 python get_torsion_scans.py
